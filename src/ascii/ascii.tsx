2fbff604ca558b1013c49d1e55699ade62e975bb
mgurga <motiurga@gmail.com>
2025-03-31 16:17:48 -0700

    added fancy animation

--- src/ascii/ascii.tsx
+++ src/ascii/ascii.tsx
@@ -6,39 +6,6 @@ function rand(min: number, max: number) {
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-function random_name_ascii(): string {
-    const num = /* rand(0, 1) */ 0;
-
-    switch(num) {
-        case 0:
-            return `
- __   __  _______  _______  ___       __   __  ______    _______  _______ 
-|  |_|  ||       ||       ||   |     |  | |  ||    _ |  |       ||   _   |
-|       ||   _   ||_     _||   |     |  | |  ||   | ||  |    ___||  |_|  |
-|       ||  | |  |  |   |  |   |     |  |_|  ||   |_||_ |   | __ |       |
-|       ||  |_|  |  |   |  |   |     |       ||    __  ||   ||  ||     
-| ||_|| ||       |  |   |  |   |     |       ||   |  | ||   |_| ||   _   |
-|_|   |_||_______|  |___|  |___|     |_______||___|  |_||_______||__| |__|
-                                                                                `;
-
-        case 1:
-            return `
- /$$      /$$             /$$     /$$       /$$   /$$                              
-| $$$    /$$$            | $$    |__/      | $$  | $$                              
-| $$$$  /$$$$  /$$$$$$  /$$$$$$   /$$      | $$  | $$  /$$$$$$   /$$$$$$   /$$$$$$ 
-| $$ $$/$$ $$ /$$__  $$|_  $$_/  | $$      | $$  | $$ /$$__  $$ /$$__  $$ |____  $$
-| $$  $$$| $$| $$  \ $$  | $$    | $$      | $$  | $$| $$  \__/| $$  \ $$  /$$$$$$$
-| $$\  $ | $$| $$  | $$  | $$ /$$| $$      | $$  | $$| $$      | $$  | $$ /$$__  $$
-| $$ \/  | $$|  $$$$$$/  |  $$$$/| $$      |  $$$$$$/| $$      |  $$$$$$$|  $$$$$$$
-|__/     |__/ \______/    \___/  |__/       \______/ |__/       \____  $$ \_______/
-                                                                /$$  \ $$          
-                                                               |  $$$$$$/          
-                                                                \______/           `;
-    }
-
-    return "Moti Urga";
-}
-
 function age() {
     return ((new Date() - new Date("08/17/2004")) / (1000*60*60*24) / 365).toFixed(5);
 }
@@ -64,8 +31,13 @@ export function AsciiApp() {
     <div>
         <noscript id="rawdata">
             <pre>
-{random_name_ascii()}
-
+ __   __  _______  _______  ___       __   __  ______    _______  _______  <br />
+|  |_|  ||       ||       ||   |     |  | |  ||    _ |  |       ||   _   | <br />
+|       ||   _   ||_     _||   |     |  | |  ||   | ||  |    ___||  |_|  | <br />
+|       ||  | |  |  |   |  |   |     |  |_|  ||   |_||_ |   | __ |       | <br />
+|       ||  |_|  |  |   |  |   |     |       ||    __  ||   ||  ||         <br />
+| ||_|| ||       |  |   |  |   |     |       ||   |  | ||   |_| ||   _   | <br />
+|_|   |_||_______|  |___|  |___|     |_______||___|  |_||_______||__| |__| <br />
 (Moti Urga)
 
 <br /> <br />
@@ -77,7 +49,7 @@ Science at the University of California Riverside.
 <br /> <br />
 
 Send me an email: <u>motiurga AT gmail DOT com</u> <br />
-View all my projects on GitHub: <a href="https://github.com/mgurga">github.com/mgurga</a>
+View all my projects on GitHub: <a target="_blank" href="https://github.com/mgurga">github.com/mgurga</a>
 
 <br /> <br />
 
@@ -85,18 +57,18 @@ Here are some of my favorite projects:
 
 <br /> <br />
 
-⭐ <a href="https://github.com/mgurga/itch">itch</a> - Modern C++ emulator for Scratch 3 projects.
+⭐ <a target="_blank" href="https://github.com/mgurga/itch">itch</a> - Modern C++ emulator for Scratch 3 projects.
 
 <br /> {taglist(["C++", "SFML", "OpenGL", "CLI", "GTest"])} <br />
 
 I originially started the project as a way of running Scratch projects faster and avoiding javascript, <br />
 but I ended up colaborating with other members in the community and combing over the <a href="https://github.com/scratchfoundation/scratch-vm">native implementation.</a> <br />
-Itch also includes a visual debugging interface that provides fine tuned control over the engine. Despite <br />
-Scratch's many quarks, itch supports the large marjority of instructions. <br />
+Itch also includes a visual debugging interface that provides fine tuned control over the engine. <br />
+Scratch many quarks, yet itch supports the large marjority of instructions. <br />
 
 <br /> <br />
 
-⭐ <a href="https://github.com/mgurga/lineinradio">lineinradio</a> - 24/7 community ran online radio station.
+⭐ <a target="_blank" href="https://github.com/mgurga/lineinradio">lineinradio</a> - 24/7 community ran online radio station.
 
 <br /> {taglist(["Python", "Django", "Fullstack", "Streaming", "CRUD", "SQLite", "Templates"])} <br />
 
@@ -104,11 +76,11 @@ Online radio station with shows submitted by users. Shows can be any length and 
 at certain times of day. Every night, the server will automatically generate a schedule for the day. <br />
 DJs are able customize their profiles by changing backgrounds, text color, banners, an profile pictures.<br />
 
-<a href="https://lineinradio.xyz/">[ Tune In ]</a> <br />
+<a target="_blank" href="https://lineinradio.xyz/">[ Tune In ]</a> <br />
 
 <br /> <br />
 
-⭐ <a href="https://github.com/mgurga/repmap">RepMap</a>, <a href="https://github.com/mgurga/repapi">repapi</a> - Interactive map to find your state representatives.
+⭐ <a target="_blank" href="https://github.com/mgurga/repmap">RepMap</a>, <a target="_blank" href="https://github.com/mgurga/repapi">repapi</a> - Interactive map to find your state representatives.
 
 <br /> {taglist(["Android", "Java", "Python", "OpenStreetMap", "API", "Scraper"])} <br />
 
@@ -120,6 +92,8 @@ list on the app. Our submission earned us 2nd place in our district. <br />
 <br /> <br />
 Thanks for reading :) <br />
 <br /> <br />
+
+<a href="#" onclick="return replay()">[ Replay animation ]</a> <br />
 
             </pre>
         </noscript>
